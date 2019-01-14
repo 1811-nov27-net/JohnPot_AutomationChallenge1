fb6959c389e92231c804c85ff7c4b2c33ed28d21
John Pot <j.pot@digipen.edu>
2019-01-14 12:58:01 -0600

    Automation assignment completed

---
+++ Registration/Registration/Controllers/RegisterController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Registration.Models;
+
+namespace Registration.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        // GET: Register/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RegisterViewModel user)
+        {
+            return RedirectToAction("Create", "Register");
+        }
+    }
+}
---
+++ Registration/Registration/Models/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Registration.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string CompanyRole { get; set; }
+    }
+}
